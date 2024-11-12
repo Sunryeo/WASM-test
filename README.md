106aaa23ecbb74ca1b1ddd470f7429f6c92fce70
Sunryeo <elma9700@gmail.com>
2024-11-12 10:08:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## WASM-test
+- wasm + next.js
---
+++ teaVM-testing/target/teaVM-testing-1.0-SNAPSHOT/teavm/src/org/example/Client.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.teavm.jso.dom.html.HTMLDocument;
+
+public class Client {
+  public static void main(String[] args) {
+    var document = HTMLDocument.current();
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("TeaVM generated element"));
+    document.getBody().appendChild(div);
+
+    System.out.println(MyClass.getPrint());
+  }
+}
